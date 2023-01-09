8c921680d3ee193716a2ebb647fa24cfa1fec749
Erick Augusto <erickaugusto506@gmail.com>
2023-01-09 16:12:54 -0300

    Testes unitários usando `assert`.

--- unittests.py
+++ unittests.py
@@ -5,23 +5,13 @@ Testes unitários para o módulo chess.py.
 from chess import mov_horse
 
 def test_mov_horse():
+
   result = mov_horse([5, 4])
-
-  if result not in [[4, 2], [6, 2], [4, 6], [6, 6], [3, 3], [3, 5], [7, 3], [7, 5]]: # d2, f2, d6, f6, c3, c5, g3, g5
-    print('Teste não passado.')
-    return False
-
+  assert result in [[4, 2], [6, 2], [4, 6], [6, 6], [3, 3], [3, 5], [7, 3], [7, 5]] # d2, f2, d6, f6, c3, c5, g3, g5
   print('Teste passado.')
 
-  '''
-  outro teste.
-  '''
   result = mov_horse([1, 1])
-
-  if result not in [[3, 2], [2, 3]]: # c2, b3
-    print('Teste não passado.')
-    return False
-
+  assert result in [[3, 2], [2, 3]] # c2, b3
   print('Teste passado.')
 
   return True
